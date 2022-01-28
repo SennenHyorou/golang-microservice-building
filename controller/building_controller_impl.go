69aca3a6e0763c483b4a8cbbb5511553b5f0fa70
SennenHyorou <chenindraxr2@gmail.com>
2022-01-28 14:55:00 +0700

    Added building controllers

---
+++ controller/building_controller_impl.go
@@ -0,0 +1,88 @@
+package controller
+
+import (
+	"github.com/SennenHyorou/golang-microservice-building/helper"
+	"github.com/SennenHyorou/golang-microservice-building/model/web"
+	"github.com/SennenHyorou/golang-microservice-building/service"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+)
+
+type BuildingControllerImpl struct {
+	BuildingService service.BuildingService
+}
+
+func (controller BuildingControllerImpl) Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	buildingCreateRequest := web.BuildingCreateRequest{}
+	helper.ReadFromRequestBody(request, &buildingCreateRequest)
+
+	buildingResponse := controller.BuildingService.Create(request.Context(), buildingCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   buildingResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+
+}
+
+func (controller BuildingControllerImpl) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	buildingUpdateRequest := web.BuildingUpdateRequest{}
+	helper.ReadFromRequestBody(request, &buildingUpdateRequest)
+
+	buildingId := params.ByName("buildingId")
+	id, err := strconv.Atoi(buildingId)
+	helper.PanicIfError(err)
+
+	buildingUpdateRequest.Id = id
+
+	buildingResponse := controller.BuildingService.Update(request.Context(), buildingUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   buildingResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller BuildingControllerImpl) Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	buildingId := params.ByName("buildingId")
+	id, err := strconv.Atoi(buildingId)
+	helper.PanicIfError(err)
+
+	controller.BuildingService.Delete(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller BuildingControllerImpl) FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	buildingId := params.ByName("buildingId")
+	id, err := strconv.Atoi(buildingId)
+	helper.PanicIfError(err)
+
+	buildingResponse := controller.BuildingService.FindById(request.Context(), id)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   buildingResponse,
+	}
+
+	helper.WriteToResponseBody(writer, webResponse)
+}
+
+func (controller BuildingControllerImpl) FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	buildingResponses := controller.BuildingService.FindAll(request.Context())
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   buildingResponses,
+	}
+	helper.WriteToResponseBody(writer, webResponse)
+}
